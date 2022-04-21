c05ece83f042cb1c157507d6c0021609e91ad080
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-04-22 00:01:07 +0900

    rails g scaffold User name:string email:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#top'
+  resources :users
+  # root 'home#top'
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
