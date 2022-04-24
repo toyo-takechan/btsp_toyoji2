9d668eb0cd90a46e61af421ed94c62c70796f9ed
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-04-24 10:25:21 +0900

    hometop header footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
-  # root 'home#top'
-  root 'users#index'
+  root 'home#top'
+  # root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
