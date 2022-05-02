fd419c123eabc02d2d4bdef07d3a52f669289248
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-05-02 23:55:05 +0900

    practce enjoy header

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
   root 'home#top'
+  get '/practice', to: 'home#practice'
+  get '/enjoy',    to: 'home#enjoy'
+  resources :users
+  resources :microposts
   # root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
